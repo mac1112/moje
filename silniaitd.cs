97f05e7303ad602db51078434a3cb84b7275aa8c
mac1112 <32835927+mac1112@users.noreply.github.com>
2017-10-25 12:33:19 +0200

    silnia

---
+++ silniaitd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zakres = 50;
+            List<int> lotki = new List<int>(zakres);
+
+            for (int i = 1; i <= zakres; i++)
+            {
+                lotki.Add(i);
+            }
+            Random nazwa = new Random();
+            for (int i = 0; i <= 6; i++)
+            {
+                
+                int liczba = nazwa.Next(0,zakres);
+                Console.WriteLine(lotki[liczba]);
+                zakres--;
+                lotki.Remove(liczba);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
